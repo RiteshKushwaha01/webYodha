3afd52ea33bb6ea210dcb0520d0f57009d54d15b
Ritesh Kushwaha <riteshkushwaha089@gmail.com>
2026-03-15 23:27:11 +0530

    06: error tracking

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -9,31 +9,46 @@ export const demoGenerate = inngest.createFunction(
   { id: 'demo-generate' },
   { event: 'demo/generate' },
   async ({ event, step }) => {
-     const { prompt } = event.data as { prompt: string }
+    const { prompt } = event.data as { prompt: string }
 
-     const urls = (await step.run('exctract-urls', async () => {
-       return prompt.match(URL_REGEX) ?? []
-     })) as string[]
+    const urls = (await step.run('exctract-urls', async () => {
+      return prompt.match(URL_REGEX) ?? []
+    })) as string[]
 
-     const scrapedContent = await step.run('scrape-urls', async () => {
-       const results = await Promise.all(
-         urls.map(async (url) => {
-           const result = await firecrawl.scrape(url, { formats: ['markdown'] })
-           return result.markdown ?? null
-         }),
-       )
-       return results.filter(Boolean).join('\n\n')
-     })
+    const scrapedContent = await step.run('scrape-urls', async () => {
+      const results = await Promise.all(
+        urls.map(async (url) => {
+          const result = await firecrawl.scrape(url, { formats: ['markdown'] })
+          return result.markdown ?? null
+        }),
+      )
+      return results.filter(Boolean).join('\n\n')
+    })
 
-     const finalPrompt = scrapedContent
-       ? `Context:\n${scrapedContent}\n\nQuestion: ${prompt}`
-       : prompt
+    const finalPrompt = scrapedContent
+      ? `Context:\n${scrapedContent}\n\nQuestion: ${prompt}`
+      : prompt
 
     await step.run('generate-text', async () => {
       return await generateText({
         model: google('gemini-2.5-flash'),
         prompt: finalPrompt,
+        experimental_telemetry: {
+          isEnabled: true,
+          recordInputs: true,
+          recordOutputs: true,
+        },
       })
+    })
+  },
+)
+
+export const demoError = inngest.createFunction(
+  { id: 'demo-error' },
+  { event: 'demo/error' },
+  async ({ step }) => {
+    await step.run('fail', async () => {
+      throw new Error('Inngest error: Background job failed!')
     })
   },
 )
