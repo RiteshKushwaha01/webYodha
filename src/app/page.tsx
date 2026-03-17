1c9ae49bf8f490c076e98a4aebef2f735485081a
Ritesh Kushwaha <riteshkushwaha089@gmail.com>
2026-03-17 19:40:09 +0530

    07: (7-9) projects, ide layout, file explorer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,7 @@
-'use client'
+import { ProjectsView } from '../features/projects/components/projects-view'
 
-import { Button } from '@/components/ui/button'
-import { useMutation, useQueries, useQuery } from 'convex/react'
-import Image from 'next/image'
-import { get } from '../../convex/projects'
-import { api } from '../../convex/_generated/api'
-
-export default function Home() {
-  const projects = useQuery(api.projects.get)
-  const createProject = useMutation(api.projects.create)
-  return (
-    <div className="flex flex-col gap-2 p-4">
-      <Button onClick={() => createProject({
-        name: "New Project 12"
-      })}>
-        Add new
-      </Button>
-      {projects?.map((project) => (
-        <div key={project._id} className="border rounded p-2 flex flex-col">
-          <p>{project.name}</p>
-          <p> Owner Id: {project.ownerId}</p>
-        </div>
-      ))}
-    </div>
-  )
+const Home = () => {
+  return <ProjectsView />
 }
+
+export default Home
